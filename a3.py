ced6c8e0ed2ed31b5d6c981e44c5a9ca408699c7
Isabel Zemke <dzemke@cps.edu>
2024-10-02 11:00:48 -0500

    Day 1

--- a3.py
+++ a3.py
@@ -22,6 +22,75 @@ from movies import movie_db
 from match import match
 from typing import List, Tuple, Callable, Any
 
+def match(pattern: List[str], source: List[str]) -> List[str]:
+    """Attempts to match the pattern to the source.
+
+    % matches a sequence of zero or more words and _ matches any single word
+
+    Args:
+        pattern - a pattern using to % and/or _ to extract words from the source
+        source - a phrase represented as a list of words (strings)
+
+    Returns:
+        None if the pattern and source do not "match" ELSE A list of matched words
+        (words in the source corresponding to _'s or %'s, in the pattern, if any)
+    """
+    sind = 0  # current index we are looking at in source list
+    pind = 0  # current index we are looking at in pattern list
+    result: List[str] = []  # to store substitutions we will return if matched
+
+    # keep checking as long as we haven't hit the end of either pattern or source while
+    # pind is still a valid index OR sind is still a valid index (valid index means that
+    # the index is != to the length of the list)
+    while sind < len(source) or pind < len(pattern):
+
+        # 1) if we reached the end of the pattern but not source
+        if  sind < len(source) and pind == len(pattern):
+            return None
+        # 2) if the current thing in the pattern is a %
+        # WARNING: this condition contains the bulk of the code for the assignment
+        # If you get stuck on this one, we encourage you to attempt the other conditions
+        #   and come back to this one afterwards
+        elif pattern[pind] == "%":
+            percentString = ""
+            if pind + 1 == len(pattern):
+                while(sind != len(source)):
+                    if(percentString != ""):
+                        percentString += " " 
+                    percentString += source[sind]
+                    sind += 1
+            else:
+                while source[sind] != pattern[pind+1]:
+                    if sind < len(source):
+                        if(percentString != ""):
+                            percentString += " " 
+                        percentString += source[sind]
+                        sind += 1
+                    else: return None
+                result.append(percentString)
+                pind += 1
+
+        # 3) if we reached the end of the source but not the pattern
+        elif sind == len(source) and pind < len(pattern):
+            return None
+        # 4) if the current thing in the pattern is an _
+        elif pattern[pind] == "_":
+            result.append(source[sind])
+            sind += 1
+            pind += 1
+        # 5) if the current thing in the pattern is the same as the current thing in the
+        # source
+        elif pattern[pind] == source[sind]:
+            sind += 1
+            pind += 1
+        # 6) else : this will happen if none of the other conditions are met it
+        # indicates the current thing it pattern doesn't match the current thing in
+        # source
+        else: 
+            return None
+
+    return result
+
 # The projection functions, that give us access to certain parts of a "movie" (a tuple)
 def get_title(movie: Tuple[str, str, int, List[str]]) -> str:
     return movie[0]
@@ -54,7 +123,12 @@ def title_by_year(matches: List[str]) -> List[str]:
     Returns:
         a list of movie titles made in the passed in year
     """
-    pass
+    year = int(matches[0])
+    result = []
+    for movie in movie_db:
+        if get_year(movie) == year:
+            result.append(get_title(movie))
+    return result
 
 
 def title_by_year_range(matches: List[str]) -> List[str]:
@@ -70,8 +144,13 @@ def title_by_year_range(matches: List[str]) -> List[str]:
         a list of movie titles made during those years, inclusive (meaning if you pass
         in ["1991", "1994"] you will get movies made in 1991, 1992, 1993 & 1994)
     """
-    pass
-
+    year1 = int(matches[0])
+    year2 = int(matches[1])
+    result = []
+    for movie in movie_db:
+        if get_year(movie) > year1 and get_year(movie) < year2:
+            result.append(get_title(movie))
+    return result
 
 def title_before_year(matches: List[str]) -> List[str]:
     """Finds all movies made before the passed in year
@@ -163,6 +242,7 @@ def title_by_actor(matches: List[str]) -> List[str]:
 
 # dummy argument is ignored and doesn't matter
 def bye_action(dummy: List[str]) -> None:
+    print("Ok, bye!")
     raise KeyboardInterrupt
 
 
